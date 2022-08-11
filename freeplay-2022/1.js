090b02bccc374fdef147a32cba245c26ea0b4515
zhenya-paitash <naitavv93@gmail.com>
2022-08-11 14:30:09 +0300

    freeplay 2022 acceleration

--- freeplay-2022/1.js
+++ freeplay-2022/1.js
@@ -1,4 +1,4 @@
-const MAX_FRAMERATE = 10
+const MAX_FRAMERATE = 6
 
 class SimpleObject {
   constructor(x, y) {
@@ -55,13 +55,52 @@ class SimpleObject {
   }
 
   dynamicMoveTo(x2, y2, duration = 1_000) {
-    // TODO реализовать перемещение с ускорением и торможением
-    return null
+    // rotation
+    this.rotate(x2, y2)
+
+    // ? допуская что 1ую половину пути он ускоряется, а 2ую тормозит
+    const frameTime = 1000 / MAX_FRAMERATE
+    const frames = (duration / 1000) * MAX_FRAMERATE
+
+    const res = Array.from({ length: frames + 1 }, (_, idx) => {
+      const time = frameTime * idx
+      const [x, y] = this.#getAccelerationCoordinates(
+        [this.x, this.y],
+        [x2, y2],
+        duration / 1000,
+        time / 1000
+      )
+
+      return { x, y, time: +time.toFixed(2) }
+    })
+
+    // В случае отсутствия координат последнего кадра
+    if (res.at(-1).time !== duration) res.push({ x: x2, y: y2, time: duration })
+
+    // move
+    this.x = x2
+    this.y = y2
+
+    return res
+  }
+
+  #getAccelerationCoordinates([x1, y1], [x2, y2], t, tnow) {
+    // Скорость V = s/t  px/sec
+    const vx = (x2 - x1) / t
+    const vy = (y2 - y1) / t
+    // Ускорение a = (v - v0) / t
+    const ax = (vx - 0) / (t / 2)
+    const ay = (vy - 0) / (t / 2)
+    // Текущие координаты y = y0 + v0 * t + ((a * t**2)/ 2)
+    const x = x1 + 0 * tnow + (ax * Math.pow(tnow, 2)) / 2
+    const y = y1 + 0 * tnow + (ay * Math.pow(tnow, 2)) / 2
+
+    return [+x.toFixed(2), Number(y.toFixed(2))]
   }
 }
 
-const obj = new SimpleObject(100, 100)
+const obj = new SimpleObject(12, 18)
 
 // console.log(obj.moveTo(10, 10, 1_000))
 // console.log(obj.rotate(0, -90))
-// console.log(obj.dynamicMoveTo(1000, 1000, 1_000))
+console.log(obj.dynamicMoveTo(500, 250, 4_400))
